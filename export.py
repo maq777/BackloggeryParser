8ce17220c2e33e10010d399f9bcfe73012d3c09c
maq777 <maq@maqibooy.com>
2021-01-02 21:48:27 +0100

    Improved output. Updated with 2020 games list.

--- export.py
+++ export.py
@@ -4,8 +4,20 @@ tree = html.parse("backloggery.html")
 for gamebox in tree.xpath('//section[@class="gamebox" and child::div[@class="gamerow"]]'):
 
     name = next(iter(gamebox.xpath('./h2/b/text()')), '')
+    dlc = next(iter(gamebox.xpath('../../h2/b/text()')), '')
     compilation = next(iter(gamebox.xpath('../../h2/b/text()[2]')), '')
     platform = next(iter(gamebox.xpath('.//div[@class="gamerow"]/b/text()')), '')
-    progress = next(iter(gamebox.xpath('(.//div[@class="gamerow"]/text())[2]')), '')
+    #progress = next(iter(gamebox.xpath('(.//div[@class="gamerow"]/text())[2]')), '')
 
-    print(f'{name}\t{compilation.strip()}\t{platform.strip()}\t{progress.strip()}')
+    dlc = dlc.strip()
+    compilation = compilation.strip()
+    platform = platform.strip()
+    #progress = progress.strip()
+
+    if dlc:
+        name = f'{dlc}: {name}'
+
+    if compilation:
+        name = f'{name} ({compilation})'
+
+    print(f'{name} [{platform}]')
